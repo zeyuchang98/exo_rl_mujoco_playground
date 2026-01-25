ba15d1b3a332f2bf1be6a9e6b3bfaf9b9230b34c
Zeyu Chang <71049259+AugustCzy@users.noreply.github.com>
2026-01-25 10:45:16 -0500

    update

--- mujoco_playground/_src/locomotion/njit_exo/joystick.py
+++ mujoco_playground/_src/locomotion/njit_exo/joystick.py
@@ -35,9 +35,9 @@ def default_config() -> config_dict.ConfigDict:
       sim_dt=0.002,
       episode_length=1000,
       action_repeat=1,
-      action_scale=0.5,
+      action_scale=0.25,
       history_len=1,
-      soft_joint_pos_limit_factor=0.95,
+      soft_joint_pos_limit_factor=1.0,
       noise_config=config_dict.create(
           level=1.0,  # Set to 0.0 to disable noise.
           scales=config_dict.create(
@@ -53,7 +53,7 @@ def default_config() -> config_dict.ConfigDict:
       reward_config=config_dict.create(
           scales=config_dict.create(
               # Tracking related rewards.
-              tracking_lin_vel=1.0,
+              tracking_lin_vel=3.0,
               tracking_ang_vel=0.5,
               # Base related rewards.
               lin_vel_z=0.0,
@@ -73,7 +73,7 @@ def default_config() -> config_dict.ConfigDict:
               # Other rewards.
               stand_still=0.0,
               alive=0.0,
-              termination=-1.0,
+              termination=0.0,
               # Pose related rewards.
               joint_deviation_knee=-0.1,
               joint_deviation_hip=-0.25,
@@ -82,7 +82,7 @@ def default_config() -> config_dict.ConfigDict:
           ),
           tracking_sigma=0.5,
           max_foot_height=0.1,
-          base_height_target=0.95,
+          base_height_target=0.92,
       ),
       push_config=config_dict.create(
           enable=True,
