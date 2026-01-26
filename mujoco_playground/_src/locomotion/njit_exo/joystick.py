3e2dbdb1c3de24c717674e8c1dd753c9a8b7f26c
Zeyu Chang <71049259+AugustCzy@users.noreply.github.com>
2026-01-26 12:46:07 -0500

    update

--- mujoco_playground/_src/locomotion/njit_exo/joystick.py
+++ mujoco_playground/_src/locomotion/njit_exo/joystick.py
@@ -53,7 +53,7 @@ def default_config() -> config_dict.ConfigDict:
       reward_config=config_dict.create(
           scales=config_dict.create(
               # Tracking related rewards.
-              tracking_lin_vel=3.0,
+              tracking_lin_vel=5.0,
               tracking_ang_vel=0.5,
               # Base related rewards.
               lin_vel_z=0.0,
